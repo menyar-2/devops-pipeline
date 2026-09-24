308955af3ad46f0453f9287b3588c8b1ccc8abb1
Menyar <menyaramari20@gmail.com>
2026-09-24 17:06:13 +0100

    Initial Maven project

---
+++ src/main/java/com/example/devops/App.java
@@ -0,0 +1,12 @@
+package com.example.devops;
+
+public class App {
+
+    public static String message() {
+        return "DevOps Pipeline works!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+}
---
+++ src/test/java/com/example/devops/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.devops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testMessage() {
+        assertEquals("DevOps Pipeline works!", App.message());
+    }
+}
